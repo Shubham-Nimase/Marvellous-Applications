891ac706c7932fcbb7d36e641cf557b2e1977c34
Shubham Ambadas Nimase <shubhamnimase229@gmail.com>
2021-06-17 12:55:45 +0530

    Add files via upload

---
+++ Program_126/MarvellousArray.java
@@ -0,0 +1,36 @@
+package Marvellous;
+
+import java.lang.*;
+import java.util.*;
+
+public class MarvellousArray
+{
+	public int arr[];					// Characteristics
+
+	public MarvellousArray(int size)	// Parametrised constructor
+	{
+		arr = new int[size];			// Dynamic memory allocation
+	}
+
+	public void Accept()				// Behaviour
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter the elements");
+
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = sobj.nextInt();
+		}	
+	}
+
+	public void Display()				// Behaviour
+	{
+		System.out.println("Entered elements are");
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i]+"\t");
+		}
+		System.out.println();	
+	}
+}
+
